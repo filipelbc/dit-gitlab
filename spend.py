e8dec3404de8e667f1c30c6f75dfc9c2220f2d8b
filipelbc <filipelbc@brickabode.com>
2019-02-25 10:30:12 -0300

    Make script use dit internals

--- spend.py
+++ spend.py
@@ -2,7 +2,6 @@
 # -*- coding: utf-8 -*-
 
 import sys
-import json
 import re
 import os
 from datetime import timedelta
@@ -13,6 +12,13 @@ from gitlab.exceptions import GitlabGetError
 from dit.utils import (
     convert_datetimes,
     interpret_date,
+    apply_filters,
+)
+from dit.common import names_to_string as _
+from dit.dit import Dit
+from dit.exceptions import (
+    ArgumentError,
+    maybe_raise_unrecognized_argument,
 )
 
 
@@ -64,29 +70,30 @@ def get_gitlab_project(group, subgroup):
     return gl.projects.get(project)
 
 
-def add_spent_time(issue, td, date):
+def add_spent_time(issue, td, date, dry_run=False):
     """
     This is a workaround because the Gitlab API does not allow specifying the
     "date".
     """
     try:
         body = to_gitlab_spend_string(td, date)
-        print(body)
-        issue.notes.create({'body': body})
+        if not dry_run:
+            print(body)
+            issue.notes.create({'body': body})
+        else:
+            print("Dry run:", body)
     except Exception as e:
         print(e)
 
 
-def spend_time_on(issue, task, since, until):
-    for l in convert_datetimes(task).get('logbook', []):
-        if l['in'] >= since and\
-           l['out'] is not None and\
-           l['out'] < until:
-            add_spent_time(
-                issue=issue,
-                td=l['out'] - l['in'],
-                date=l['in'].date()
-            )
+def spend_time_on(issue, logbook, dry_run):
+    for log in logbook:
+        add_spent_time(
+            issue=issue,
+            td=log['out'] - log['in'],
+            date=log['in'].date(),
+            dry_run=dry_run,
+        )
 
 
 def get_gitlab_issue(iid, gl_project):
@@ -100,39 +107,68 @@ def get_gitlab_issue(iid, gl_project):
 # ========================================
 # Main
 
+def get_dit():
+    dit = Dit()
+    dit._setup_base_path(None)
+    dit._load_current()
+    dit._load_previous()
+    dit.index.load(dit.base_path)
+    return dit
 
-def cli(base_path, group, subgroup, task, since, until):
-    since = interpret_date(since)
-    until = interpret_date(until)
 
-    gl_project = get_gitlab_project(group, subgroup)
+def cli(argv):
+    dit = get_dit()
+
+    dry_run = False
+    filters = {}
+
+    while len(argv) > 0 and argv[0].startswith("-"):
+        opt = argv.pop(0)
+        if opt in ["--dry-run"]:
+            dry_run = True
+        elif opt in ["--from"]:
+            filters["from"] = interpret_date(argv.pop(0))
+        elif opt in ["--to"]:
+            filters["to"] = interpret_date(argv.pop(0))
+        else:
+            raise ArgumentError("No such option: %s" % opt)
+
+    if len(argv) < 1:
+        raise ArgumentError("Missing argument.")
+
+    (group, subgroup, task) = dit._backward_parser(argv, throw=False)
+    maybe_raise_unrecognized_argument(argv)
+    print('Selected: %s' % _(group, subgroup, task))
+
+    match = re.match(r'^(?P<type>i|m)(?P<id>\d+)$', task)
+    if not match:
+        print("Unrecognized task name pattern:", task)
+        return
+
+    data = dit._load_task_data(group, subgroup, task)
+    logbook = apply_filters(convert_datetimes(data), filters).get('logbook', [])
+
+    if not logbook:
+        print("Nothing to spend.")
+        return
+
+    gl_project = get_gitlab_project(group or ".", subgroup or ".")
     if not gl_project:
         print("Could not choose a project to use.")
         return
 
-    match = re.match(r'^(?P<type>i|m)(?P<id>\d+)$', task)
-    if not match:
-        print("Unrecognized task name pattern.")
+    iid = int(match.group('id'))
+    issue = get_gitlab_issue(iid, gl_project)
+    if not issue:
+        print("No such issue:", iid)
         return
 
-    issue = get_gitlab_issue(int(match.group('id')), gl_project)
-    if issue is None:
-        return
-
-    task_fp = os.path.join(base_path, group, subgroup, task)
-    with open(task_fp, 'r') as f:
-        task = json.load(f)
-
-    spend_time_on(issue, task, since, until)
-
+    spend_time_on(issue, logbook, dry_run)
 
 # ========================================
 # Invocation
 
 
 if __name__ == "__main__":
-    if len(sys.argv) != 7:
-        raise Exception("ERROR: Invalid arguments: %s" % sys.argv)
     sys.argv.pop(0)
-
-    cli(*sys.argv)
+    cli(sys.argv)
